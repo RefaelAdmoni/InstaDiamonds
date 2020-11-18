a51bb963b52869cf137a36c98dc825752b13b706
RefaelAdmoni <rafi25ad@gmail.com>
2020-11-18 14:33:29 +0200

    Init the project

--- app/src/main/java/com/example/instadiamond/UserProfile.java
+++ app/src/main/java/com/example/instadiamond/UserProfile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class UserProfile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_Page);
+        setContentView(R.layout.activity_user_profile);
     }
 }
